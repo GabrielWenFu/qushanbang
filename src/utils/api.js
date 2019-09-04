5de0d34922dc51a4609c8d5719ac2e56d7e5dc90
guowenfu <18310535167@163.com>
2019-09-04 14:32:45 +0800

    提交代码

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,36 +2,31 @@ const Api = {
   _core: (options) => {
     return new Promise((resolve, reject) => {
       wx.request(
-        Object.assign({
+        Object.assign(options, {
           header: {
-            'content-type': 'application/json'
+            'content-type': 'application/x-www-form-urlencoded'
           },
+          method: 'post',
           success: resolve,
           fail: reject
-        }, options))
+        }))
     })
   },
   _qqTopList (id = 26) {
     const options = {
-      url: `https://v1.itooi.cn/tencent/topList?id=${id}&pageSize=500&page=0&format=1`
-    }
-    return this._core(options)
-  },
-  _qqSong (id) {
-    const options = {
-      url: `https://v1.itooi.cn/tencent/song?id=${id}`
-    }
-    return this._core(options)
-  },
-  _qqUrl (id) {
-    const options = {
-      url: `https://v1.itooi.cn/tencent/url?id=${id}`
+      url: `http://www.guowenfu.com/tencent/music_list`,
+      data: {
+        id
+      }
     }
     return this._core(options)
   },
   _qqLrc (id) {
     const options = {
-      url: `https://v1.itooi.cn/tencent/lrc?id=${id}`
+      url: `http://www.guowenfu.com/tencent/lrc`,
+      data: {
+        id
+      }
     }
     return this._core(options)
   }
